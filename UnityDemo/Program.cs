12b3e6476a86424764923000bfcf3a38261bbd98
朱聪 <zhucon@shanghai3h.com>
2018-02-02 09:34:15 +0800

    proj add

---
+++ UnityDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //UnityHelper.TestTransientLifetimeManager();
+
+                //Console.WriteLine("####");
+                //UnityHelper.TestContainerControlledLifetimeManager();
+
+                //Console.WriteLine("####");
+                //UnityHelper.TestHierarchicalLifetimeManager();
+
+                //Console.WriteLine("####");
+                //UnityHelper.TestExternallyControlledLifetimeManager();
+
+                //Console.WriteLine("####");
+                //UnityHelper.TestPerThreadLifetimeManager();
+
+                Console.WriteLine("####");
+                UnityHelper.TestPerResolveLifetimeManager();
+
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
